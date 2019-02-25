dd97e7efec974207274859cb9fdac6f3ec01d638
Peter Nguyen <peternguyen160@gmail.com>
2019-02-25 04:00:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ def word_substituter (string)
   array = string.split(" ")
   array.each_with_index do |words,index|
     dictionary.keys.each do |x|
-      if x == words 
+      if x == words
       array[index] = dictionary[x]
       end
       end
@@ -46,7 +46,7 @@ end
  def selective_tweet_shortener (tweet)
 if tweet.length > 140
   word_substituter(tweet)
-else 
+else
  tweet
 end
 end
@@ -60,3 +60,4 @@ end
   else
    tweet
   end
+end
